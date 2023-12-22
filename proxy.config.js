3741125fa861824c90e603c41422adf062f2f5e5
Konstantin <Lunoparc69@gmail.com>
2023-12-22 09:36:42 +0300

    init

--- proxy.config.js
+++ proxy.config.js
@@ -1,14 +1,10 @@
 
 const PROXY_CONFIG = {
   '/api': {
-    target: "http://localhost:5133/",
+    target: "http://84.54.44.140/",
     secure: false,
     logLevel: 'debug',
     changeOrigin: true,
-    pathRewrite: {
-      "^/api": ""
-    }
   },
 };
-
 module.exports = PROXY_CONFIG;
