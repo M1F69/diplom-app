6eebe354f831f7875ebc389b6c19c450870afb8e
Konstantin <Lunoparc69@gmail.com>
2023-12-22 11:44:29 +0300

    init

--- src/app/components/movie-card/movie-details/movie-details.component.ts
+++ src/app/components/movie-card/movie-details/movie-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, inject, Input, Output, SecurityContext} from "@angular/core";
+import {Component, EventEmitter, inject, Input, OnInit, Output, SecurityContext} from "@angular/core";
 import {DxPopupModule, DxTemplateModule} from "devextreme-angular";
 import {MovieCoverComponent} from "../../movie-cover";
 import {DomSanitizer} from "@angular/platform-browser";
@@ -13,11 +13,15 @@ import {DomSanitizer} from "@angular/platform-browser";
   ],
   templateUrl: './movie-details.component.html'
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
   @Input() value: any;
   @Output() close = new EventEmitter();
 
   sanitizer = inject(DomSanitizer);
+
+  ngOnInit() {
+    this.value.trailerHref = this.sanitizeUrl(this.value.trailerHref)
+  }
 
   sanitizeUrl(url: string) {
     const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
